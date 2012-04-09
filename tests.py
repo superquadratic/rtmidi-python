a4ee6a33dc83e54b80a8f2c7b0db8f510d951861
Guido Lorenz <code@superquadratic.net>
2012-04-09 21:13:10 +0200

    Added basic tests
    These tests cover port opening and closing, as well as sending and receiving
    messages (both by polling and using a callback). While this is already the
    better part of the API, I think issue #1 shouldn't be closed yet.

---
+++ tests.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import time
+import unittest
+
+import rtmidi
+
+class RtMidiTestCase(unittest.TestCase):
+
+    NOTE_ON = [0x90, 48, 100]
+    NOTE_OFF = [0x80, 48, 16]
+    TEST_PORT_NAME = 'rtmidi test port'
+    DELAY = 0.1
+
+    def setUp(self):
+        self.midi_out = rtmidi.MidiOut()
+        self.midi_out.open_virtual_port(self.TEST_PORT_NAME)
+
+        self.midi_in = rtmidi.MidiIn()
+        for i in range(self.midi_in.get_port_count()):
+            if self.midi_in.get_port_name(i) == self.TEST_PORT_NAME:
+                self.midi_in.open_port(i)
+                break
+
+    def tearDown(self):
+        self.midi_in.close_port()
+        self.midi_in = None
+        self.midi_out.close_port()
+        self.midi_out = None
+
+    def test_send_and_get_message(self):
+        self.midi_out.send_message(self.NOTE_ON)
+        self.midi_out.send_message(self.NOTE_OFF)
+        time.sleep(self.DELAY)
+        incoming_message_1, _ = self.midi_in.get_message()
+        incoming_message_2, _ = self.midi_in.get_message()
+        self.assertEqual(incoming_message_1, self.NOTE_ON)
+        self.assertEqual(incoming_message_2, self.NOTE_OFF)
+
+    def test_callback(self):
+        incoming_messages = []
+        def callback(message, time_stamp):
+            incoming_messages.append(message)
+        self.midi_in.set_callback(callback)
+        self.midi_out.send_message(self.NOTE_ON)
+        self.midi_out.send_message(self.NOTE_OFF)
+        time.sleep(self.DELAY)
+        self.assertEqual(incoming_messages, [self.NOTE_ON, self.NOTE_OFF])
+
+if __name__ == '__main__':
+    unittest.main()
