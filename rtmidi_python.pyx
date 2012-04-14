940fb6e0c7b59be6eb002460db5bd2873dd52133
Guido Lorenz <code@superquadratic.net>
2012-04-15 00:05:55 +0200

    Use RtMidi base class for Python module
    This eliminates duplicate (wrapper) code.

--- rtmidi_python.pyx
+++ rtmidi_python.pyx
@@ -11,51 +11,54 @@ PyEval_InitThreads()
 cdef extern from "RtMidi/RtMidi.h":
     ctypedef void (*RtMidiCallback)(double timeStamp, vector[unsigned char]* message, void* userData)
 
-    cdef cppclass RtMidiIn:
-        RtMidiIn(string clientName, unsigned int queueSizeLimit)
+    cdef cppclass RtMidi:
         void openPort(unsigned int portNumber, string portName)
         void openVirtualPort(string portName)
-        void setCallback(RtMidiCallback callback, void* userData)
-        void cancelCallback()
-        void closePort()
         unsigned int getPortCount()
         string getPortName(unsigned int portNumber)
+        void closePort()
+
+    cdef cppclass RtMidiIn(RtMidi):
+        RtMidiIn(string clientName, unsigned int queueSizeLimit)
+        void setCallback(RtMidiCallback callback, void* userData)
+        void cancelCallback()
         void ignoreTypes(bint midiSysex, bint midiTime, bint midiSense)
         double getMessage(vector[unsigned char]* message)
 
-    cdef cppclass RtMidiOut:
+    cdef cppclass RtMidiOut(RtMidi):
         RtMidiOut(string clientName)
-        void openPort(unsigned int portNumber, string portName)
-        void closePort()
-        void openVirtualPort(string portName)
-        unsigned int getPortCount()
-        string getPortName(unsigned int portNumber)
         void sendMessage(vector[unsigned char]* message)
+
+cdef class MidiBase:
+    cdef RtMidi* baseptr(self):
+        return NULL
+    def open_port(self, port_number=0, port_name="RtMidi"):
+        self.baseptr().openPort(port_number, string(<char*>port_name))
+    def open_virtual_port(self, port_name="RtMidi"):
+        self.baseptr().openVirtualPort(string(<char*>port_name))
+    def get_port_count(self):
+        return self.baseptr().getPortCount()
+    def get_port_name(self, port_number=0):
+        return self.baseptr().getPortName(port_number).c_str()
+    def close_port(self):
+        self.baseptr().closePort()
 
 cdef void midi_in_callback(double time_stamp, vector[unsigned char]* message_vector, void* py_callback) with gil:
     message = [message_vector.at(i) for i in range(message_vector.size())]
     (<object>py_callback)(message, time_stamp)
 
-cdef class MidiIn:
+cdef class MidiIn(MidiBase):
     cdef RtMidiIn* thisptr
     def __cinit__(self, client_name="RtMidi Input Client", queue_size_limit=100):
         self.thisptr = new RtMidiIn(string(<char*>client_name), queue_size_limit)
     def __dealloc__(self):
         del self.thisptr
-    def open_port(self, port_number=0, port_name="RtMidi Input"):
-        self.thisptr.openPort(port_number, string(<char*>port_name))
-    def open_virtual_port(self, port_name="RtMidi Input"):
-        self.thisptr.openVirtualPort(string(<char*>port_name))
+    cdef RtMidi* baseptr(self):
+        return self.thisptr
     def set_callback(self, callback):
         self.thisptr.setCallback(midi_in_callback, <void*>callback)
     def cancel_callback(self):
         self.thisptr.cancelCallback()
-    def close_port(self):
-        self.thisptr.closePort()
-    def get_port_count(self):
-        return self.thisptr.getPortCount()
-    def get_port_name(self, port_number=0):
-        return self.thisptr.getPortName(port_number).c_str()
     def ignore_types(self, midi_sysex=True, midi_time=True, midi_sense=True):
         self.thisptr.ignoreTypes(midi_sysex, midi_time, midi_sense)
     def get_message(self):
@@ -67,22 +70,14 @@ cdef class MidiIn:
         else:
             return None, None
 
-cdef class MidiOut:
+cdef class MidiOut(MidiBase):
     cdef RtMidiOut* thisptr
     def __cinit__(self, client_name="RtMidi Output Client"):
         self.thisptr = new RtMidiOut(string(<char*>client_name))
     def __dealloc__(self):
         del self.thisptr
-    def open_port(self, port_number=0, port_name="RtMidi Output"):
-        self.thisptr.openPort(port_number, string(<char*>port_name))
-    def close_port(self):
-        self.thisptr.closePort()
-    def open_virtual_port(self, port_name="RtMidi Output"):
-        self.thisptr.openVirtualPort(string(<char*>port_name))
-    def get_port_count(self):
-        return self.thisptr.getPortCount()
-    def get_port_name(self, port_number=0):
-        return self.thisptr.getPortName(port_number).c_str()
+    cdef RtMidi* baseptr(self):
+        return self.thisptr
     def send_message(self, message):
         cdef vector[unsigned char]* message_vector = new vector[unsigned char]()
         for byte in message:
